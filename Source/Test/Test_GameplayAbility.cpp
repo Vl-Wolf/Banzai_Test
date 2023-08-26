47bab11e6f57bf9a22ff32fff42e5c92f2b11dfb
Vladislav <vvd6565@gmail.com>
2023-08-26 17:27:22 +0300

    Added Character animation and first configuration the ability system

---
+++ Source/Test/Test_GameplayAbility.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test_GameplayAbility.h"
+
+UTest_GameplayAbility::UTest_GameplayAbility()
+{
+	
+}
---
+++ Source/Test/Test_GameplayAbility.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Test.h"
+#include "Abilities/GameplayAbility.h"
+#include "Test_GameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API UTest_GameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UTest_GameplayAbility();
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="Ability")
+	EGASAbilityInputID AbilityInputID = EGASAbilityInputID::None;
+	
+};
